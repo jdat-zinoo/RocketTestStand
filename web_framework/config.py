61072e6a781a0c961fe790bf5717267e81a9cc99
KMPackages <kristians@mihelsons.com>
2017-03-06 18:56:02 +0200

    Web framework
    Still shitloads to do but it's a start

---
+++ web_framework/config.py
@@ -0,0 +1,10 @@
+TEMPLATE_BASE_DIR = 'resources/templates'
+
+SASS_DIR = 'resources/sass'
+SASS_OUTPUT_DIR = 'static/css'
+
+JS_DIR = 'resources/js'
+JS_REGISTER = 'resources/registers/js_registry.json'
+JS_OUTPUT = 'static/js'
+
+VIEW_REGISTRY = 'resources/registers/view_registry.json'
